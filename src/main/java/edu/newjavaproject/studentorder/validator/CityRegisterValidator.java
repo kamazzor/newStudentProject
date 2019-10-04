8793de65971176c74293b6431d791e399139c176
Mikhail Kozlov <kozlov.m.yu@yandex.ru>
2019-10-04 05:24:21 +0300

    28) Add handling other exceptions in CityRegisterValidator

--- src/main/java/edu/newjavaproject/studentorder/validator/CityRegisterValidator.java
+++ src/main/java/edu/newjavaproject/studentorder/validator/CityRegisterValidator.java
@@ -67,6 +67,10 @@ public class CityRegisterValidator {
             ex.printStackTrace();
             status = AnswerCityRegisterItem.CityStatus.ERROR;
             error = new AnswerCityRegisterItem.CityError(IN_CODE, ex.getMessage());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            status = AnswerCityRegisterItem.CityStatus.ERROR;
+            error = new AnswerCityRegisterItem.CityError(IN_CODE, ex.getMessage());
         }
         AnswerCityRegisterItem ans = new AnswerCityRegisterItem(status, person, error);
         return ans;
