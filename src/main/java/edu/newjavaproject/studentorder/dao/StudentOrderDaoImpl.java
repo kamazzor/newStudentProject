700c1a17465af17666d761e2fb850c7a82a5b782
Mikhail Kozlov <kozlov.m.yu@yandex.ru>
2019-10-09 15:48:15 +0300

    50) (training) Try use stmt.executeBatch() with limit on operations in one Batch

--- src/main/java/edu/newjavaproject/studentorder/dao/StudentOrderDaoImpl.java
+++ src/main/java/edu/newjavaproject/studentorder/dao/StudentOrderDaoImpl.java
@@ -113,10 +113,19 @@ public class StudentOrderDaoImpl implements StudentOrderDao{
      */
     private void saveChildren(Connection con, StudentOrder so, Long soId) throws SQLException{
         try (PreparedStatement stmt = con.prepareStatement(INSERT_CHILD)) {
+            int counter = 0;
             for (Child child : so.getChildren()) {
                 stmt.setLong(1, soId);
                 setParamsForChild(stmt, child);
-                stmt.executeUpdate();
+                stmt.addBatch();
+                counter++;
+                if (counter > 10000) {
+                    stmt.executeBatch();
+                    counter = 0;
+                }
+            }
+            if (counter > 0) {
+                stmt.executeBatch();
             }
         }
     }
