97419b79e624fa4f40c813a65c66b85494022cc0
Mikhail Kozlov <kozlov.m.yu@yandex.ru>
2019-10-17 02:56:36 +0300

    66) Add first example Unit-test

---
+++ src/test/java/edu/javaproject/studentorder/dao/DictionaryDaoImplTest.java
@@ -0,0 +1,11 @@
+package edu.javaproject.studentorder.dao;
+
+import org.junit.Test;
+
+public class DictionaryDaoImplTest {
+
+    @Test
+    public void testExample(){
+        System.out.println("TEST");
+    }
+}
