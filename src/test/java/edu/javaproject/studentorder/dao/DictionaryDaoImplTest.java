2ae0ddad59484c511968e7c4fa4a9728dadae2a9
Mikhail Kozlov <kozlov.m.yu@yandex.ru>
2019-10-17 18:27:04 +0300

    67) Write simple runner of unit-tests (without JUnit 4)

--- src/test/java/edu/javaproject/studentorder/dao/DictionaryDaoImplTest.java
+++ src/test/java/edu/javaproject/studentorder/dao/DictionaryDaoImplTest.java
@@ -1,11 +1,25 @@
 package edu.javaproject.studentorder.dao;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class DictionaryDaoImplTest {
 
     @Test
-    public void testExample(){
-        System.out.println("TEST");
+    public void testExample1(){
+        System.out.println("TEST 1");
     }
+
+    @Test
+    @Ignore
+    public void testExample2(){
+        System.out.println("TEST 2");
+    }
+
+    @Test
+    public void testExample3(){
+        System.out.println("TEST 3");
+        throw new RuntimeException("BAAAAD RESULT");
+    }
+
 }
